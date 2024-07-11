13fd21a34bac432f7b4e54d19c90764a5b255ca6
Divyanshu Verma <divyanshudverma@gmail.com>
2024-07-11 19:15:49 +0530

    DV

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -88,7 +88,7 @@ const NewsFeed = () => {
         <div className="h-16" />
 
         <div className="flex flex-col md:flex-row">
-          <div className="w-full p-10  md:px-10   ">
+          <div className="w-full p-10  md:px-5   ">
             <h1 className="text-3xl font-bold mb-4">Latest News</h1>
             <p className="text-lg text-gray-600 mb-8">
               Stay updated with the latest news and articles.
@@ -127,7 +127,7 @@ const NewsFeed = () => {
                 </button>
               </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10 md:px-20">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-10 md:px-8">
               {cardData.map((card, index) => (
                 <div
                   key={index}
